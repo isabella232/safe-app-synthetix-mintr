95fc6d97a8d8df33e25d07f1dd81e91e10f758b6
Ramiro Vazquez <ramirovazquez@gmail.com>
2020-07-22 20:05:39 -0300

    use synthetix-js fork without walletconnect lib (#21)
    This prevents a runtime exception when deployed to the Gnosis Safe as it get run in an iframe and the walletconnect lib uses localstorage directly

--- src/helpers/contracts/synthSummary.ts
+++ src/helpers/contracts/synthSummary.ts
@@ -8,61 +8,23 @@ type Contract = {
 export default {
   addresses: {
     1: '0x0D69755e12107695E544842BF7F61D9193f09a54',
-    3: '0x15dfbBE6Ae0BA2B27d2E9Adb54C014C388255BE9',
-    4: '0x4ec3cb1d571Cd3B7A3B3C99EC6dCa02610bd6f1e',
+    3: '0x26a3C1878c4f3598e58f8bFB60B3cD9EF083e892',
+    4: '0xba6c0220157008cecb7364b37e27885e7b5be74a',
     42: '0xA2b5742922ae4CA1676349009E33DA5fB4D05dCB'
   },
   abi: [
     {
-      constant: true,
-      inputs: [
-        { name: 'account', type: 'address' },
-        { name: 'currencyKey', type: 'bytes32' }
-      ],
-      name: 'totalSynthsInKey',
-      outputs: [{ name: 'total', type: 'uint256' }],
+      inputs: [{ internalType: 'address', name: 'resolver', type: 'address' }],
       payable: false,
-      stateMutability: 'view',
-      type: 'function'
+      stateMutability: 'nonpayable',
+      type: 'constructor'
     },
     {
       constant: true,
       inputs: [],
-      name: 'synthsRates',
+      name: 'addressResolverProxy',
       outputs: [
-        { name: '', type: 'bytes32[]' },
-        { name: '', type: 'uint256[]' }
-      ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'exchangeRates',
-      outputs: [{ name: '', type: 'address' }],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'synthetix',
-      outputs: [{ name: '', type: 'address' }],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [{ name: 'account', type: 'address' }],
-      name: 'synthsBalances',
-      outputs: [
-        { name: '', type: 'bytes32[]' },
-        { name: '', type: 'uint256[]' },
-        { name: '', type: 'uint256[]' }
+        { internalType: 'contract IAddressResolver', name: '', type: 'address' }
       ],
       payable: false,
       stateMutability: 'view',
@@ -72,19 +34,60 @@ export default {
       constant: true,
       inputs: [],
       name: 'frozenSynths',
-      outputs: [{ name: '', type: 'bytes32[]' }],
+      outputs: [{ internalType: 'bytes32[]', name: '', type: 'bytes32[]' }],
       payable: false,
       stateMutability: 'view',
       type: 'function'
     },
     {
-      inputs: [
-        { name: '_synthetix', type: 'address' },
-        { name: '_exchangeRates', type: 'address' }
+      constant: true,
+      inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+      name: 'synthsBalances',
+      outputs: [
+        { internalType: 'bytes32[]', name: '', type: 'bytes32[]' },
+        { internalType: 'uint256[]', name: '', type: 'uint256[]' },
+        { internalType: 'uint256[]', name: '', type: 'uint256[]' }
       ],
       payable: false,
-      stateMutability: 'nonpayable',
-      type: 'constructor'
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'synthsRates',
+      outputs: [
+        { internalType: 'bytes32[]', name: '', type: 'bytes32[]' },
+        { internalType: 'uint256[]', name: '', type: 'uint256[]' }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'synthsTotalSupplies',
+      outputs: [
+        { internalType: 'bytes32[]', name: '', type: 'bytes32[]' },
+        { internalType: 'uint256[]', name: '', type: 'uint256[]' },
+        { internalType: 'uint256[]', name: '', type: 'uint256[]' }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [
+        { internalType: 'address', name: 'account', type: 'address' },
+        { internalType: 'bytes32', name: 'currencyKey', type: 'bytes32' }
+      ],
+      name: 'totalSynthsInKey',
+      outputs: [{ internalType: 'uint256', name: 'total', type: 'uint256' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
     }
   ]
 } as Contract;
